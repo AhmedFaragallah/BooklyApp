3ab059cb36e50868b890cad6156ef41fabb7408e
AhmedFaragallah <ahmedmahmoudfaragallah@gmail.com>
2023-06-11 05:03:51 +0300

    add custom scroll view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -14,63 +14,73 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        const Padding(
-          padding: kPadding,
-          child: CustomBookDetailsAppBar(),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .27),
-          child: const FeaturedListViewItem(),
-        ),
-        const SizedBox(
-          height: 43,
-        ),
-        const Text(
-          'The Jungle Book',
-          style: Styles.textStyle30,
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        Opacity(
-          opacity: .7,
-          child: Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              const Padding(
+                padding: kPadding,
+                child: CustomBookDetailsAppBar(),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .27),
+                child: const FeaturedListViewItem(),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text(
+                'The Jungle Book',
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const BookingRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const BooksAction(),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle16
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const SimilarBooksListView(),
+              const SizedBox(
+                height: 40,
+              )
+            ],
           ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        const BookingRating(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        const SizedBox(
-          height: 37,
-        ),
-        const BooksAction(),
-        const SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        const SimilarBooksListView(),
-        const SizedBox(
-          height: 40,
         )
       ],
     );
