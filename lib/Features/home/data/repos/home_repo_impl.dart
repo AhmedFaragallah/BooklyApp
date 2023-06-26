2d434b43ae96c71c3a048111cbe9d5a787948a11
AhmedFaragallah <ahmedmahmoudfaragallah@gmail.com>
2023-06-26 05:28:44 +0300

    create Server Failure from dio Error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,10 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,45 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errorMassage;
+
+  const Failure(this.errorMassage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMassage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connection Timeout Was TimeOut');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout Was TimeOut');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout Was TimeOut');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate Was TimeOut');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer Was Cancel ');
+      case DioExceptionType.connectionError:
+        return ServerFailure('There Was ConnectionError With ApiServer ');
+      case DioExceptionType.unknown:
+        return ServerFailure('Unknown Error , Please Try again Later');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your Request not Found , Please try Later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error , Please try Later');
+    } else {
+      return ServerFailure('Opps There Was An Error , Please try Again ');
+    }
+  }
+}
